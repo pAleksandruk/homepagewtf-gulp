5b04b53a9ecef63d3757fae94f0981091eb39bb3
pAleksandruk <aleksandruk.p@gmail.com>
2019-07-22 23:07:19 +0200

    fix: css converted to sass, added media files, css, js and index file from homepagewtf

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,49 @@ if ('serviceWorker' in navigator) {
 
 console.log(`Hello world!`);
 
+// User welcome 
+const welcome = (name, age) => {
+  console.log(`Witaj ${name}, masz ${age}`)
+}
+
+// welcome('Pav', 37 )
+
+const myName = 'Pav';
+const myAge = 37;
+
+welcome(myName, myAge);
+welcome('Pav', 37);
+
+const button = document.querySelector('.header_button--js');
+console.log(button);
+
+function handleClick(e) {
+  console.log(e);
+  console.log('halo')
+}
+//metoda 1
+//button.addEventListener('click', handleClick);
+
+//metoda 2
+button.addEventListener('click', (e) => {
+  console.log(e.target);
+  console.log('hello arrow');
+  const header = document.querySelector('.header__title--js');
+  header.innerHTML = 'klik klik';
+  header.classList.toggle('header__title--red');
+  if (header.classList.contains('header__title--red')) {
+    console.log('jest klasa');
+  } else {
+    console.log('nie ma klasy');
+  }
+  //alert('hello');
+});
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js')
+
+navigationSwitcher.addEventListener('click', (e) => {
+  const navigationList = document.querySelector('.navigation__list--js');
+  navigationList.classList.toggle('navigation__list--visable');
+});
+
 
